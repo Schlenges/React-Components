1c5f2a1cab53072ad792b393340682bc156abbf7
Schlenges <meike_hankewicz@yahoo.de>
2020-08-20 23:39:11 +0200

    cleanup

--- clock_weather_cards/src/components/Cards.js
+++ clock_weather_cards/src/components/Cards.js
@@ -4,22 +4,22 @@ import './cards.css'
 
 const Cards = () => {
   let denver = ["America/Denver", "Denver", ["39.7", "-104"]]
-  let wiesbaden = ["Europe/Berlin", "Frankfurt", ["50", 8.3]] //Llanfairpwllgwyngyll
+  let frankfurt = ["Europe/Berlin", "Frankfurt", ["50", "8.3"]]
   let paris = ["Europe/Amsterdam", "Amsterdam", ["52", "4.9"]]
   let dublin = 	["Europe/Dublin", "Dublin", ["53", "-6"]]
   /* let batumi = ["Asia/Tbilisi", "Batumi", ["41.6", "41.6"]]
   let venezuela = ["America/Caracas", "Venezuela", ["7.6", "-65.8"]]
   let buenosAires = ["America/Argentina/Buenos_Aires", "Buenos Aires", ["-34.6", "-58"]] */
+  //Llanfairpwllgwyngyll
   
 
   return (
     <div className="cards">
       <Card timezone={denver[0]} city={denver[1]} coord={denver[2]} />
-      <Card timezone={wiesbaden[0]} city={wiesbaden[1]} coord={wiesbaden[2]} />
+      <Card timezone={frankfurt[0]} city={frankfurt[1]} coord={frankfurt[2]} />
       <div className="break"></div>
       <Card timezone={paris[0]} city={paris[1]} coord={paris[2]} />
       <Card timezone={dublin[0]} city={dublin[1]} coord={dublin[2]} />
-      {/* <Card timezone={buenosAires[0]} city={buenosAires[1]} coord={buenosAires[2]} /> */}
     </div>
   )
 }
